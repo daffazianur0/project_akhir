7ec1106a4da6c681c9ff5a58dd7a5e482ac51944
Alip-miftah <alipmiftahfauzi24@gmail.com>
2024-10-28 10:48:45 +0700

    lagi lagi mas

--- resources/views/Template/footer.blade.php
+++ resources/views/Template/footer.blade.php
@@ -1,8 +1,5 @@
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="py-6 px-6 text-center">
-        <p class="mb-0 fs-4">Design and Developed by <a href="https://adminmart.com/" target="_blank" class="pe-1 text-primary text-decoration-underline">AdminMart.com</a> Distributed by <a href="https://themewagon.com">ThemeWagon</a></p>
       </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2020 <a href="https://adminlte.io">Aplikasi Presensi Sederhana</a>.</strong> All rights reserved.
+    <p class="mb-5 fs-4 text-center">Copyright &copy; 2024 <a href="https://smkpertiwikng.sch.id/">SmkPertiwiKng </a>. All rights reserved.
 </footer>
