7568eb1a4469dbadfa85a6037e5857243c4dcfcf
Alip-miftah <alipmiftahfauzi24@gmail.com>
2024-10-30 10:41:33 +0700

    alip

--- app/Http/Controllers/Admin/RekeningController.php
+++ app/Http/Controllers/Admin/RekeningController.php
@@ -12,7 +12,7 @@ class RekeningController extends Controller
      */
     public function index()
     {
-        return view('Admin.rekening');
+        return view ('Admin.rekening');
     }
 
     /**
@@ -20,7 +20,7 @@ class RekeningController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
